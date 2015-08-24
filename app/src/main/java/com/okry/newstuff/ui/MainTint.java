0b6cb3f16adf8264d22cc0cbd2a04be53050956e
pinguo-marui <marui@camera360.com>
2015-08-24 11:14:07 +0800

    Initial commit

---
+++ app/src/main/java/com/okry/newstuff/ui/MainTint.java
@@ -0,0 +1,17 @@
+package com.okry.newstuff.ui;
+
+import android.os.Bundle;
+
+import com.okry.newstuff.BaseActivity;
+
+/**
+ * Created by mr on 15/8/24.
+ */
+public class MainTint extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}

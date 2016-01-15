bc83ac9791a1ef7e9a3cc40459ce9209e7a5a8c7
hexiaogang <hexiaogang@camera360.com>
2016-01-15 18:29:10 +0800

    smile face

--- app/src/main/java/com/okry/newstuff/activity/WebViewCacheTestActivity.java
+++ app/src/main/java/com/okry/newstuff/activity/WebViewCacheTestActivity.java
@@ -1,22 +1,32 @@
 package com.okry.newstuff.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.okry.newstuff.R;
+import com.orhanobut.logger.Logger;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class WebViewCacheTestActivity extends AppCompatActivity {
+    @InjectView(R.id.webview_hide)
+    WebView mWebViewHidden;
+
     @InjectView(R.id.webview)
     WebView mWebView;
+    @InjectView(R.id.content_layout)
+    ViewGroup mContentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +45,107 @@ public class WebViewCacheTestActivity extends AppCompatActivity {
             }
         });
         initWebView();
+        setClient();
+        initWebView2();
+        setClient2();
+        //mWebViewHidden.loadUrl("http://www.baidu.com");
+        //mWebViewHidden.loadUrl("https://mall.camera360.com/Ebusiness/views/index.html");
+        //mWebViewHidden.loadUrl("http://map.baidu.com/mobile/webapp/index/index#index/index/foo=bar/vt=map");
+        mWebViewHidden.loadUrl("http://union.co/");
+        //mWebViewHidden.loadUrl("http://joomla.templaza.net/everline/");
+        //mWebViewHidden.loadUrl("http://www.mug.pl/");
+        mWebViewHidden.setVisibility(View.INVISIBLE);
+        mWebView.setVisibility(View.INVISIBLE);
     }
 
     private void initWebView() {
+        mWebViewHidden.getSettings().setJavaScriptEnabled(true);
+
+        mWebViewHidden.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        //开启 DOM storage API 功能 
+        mWebViewHidden.getSettings().setDomStorageEnabled(true);
+
+        String cachePath = getCacheDir().getAbsolutePath();
+        //设置  Application Caches 缓存目录 
+        mWebViewHidden.getSettings().setAppCachePath(cachePath);
+        mWebViewHidden.getSettings().setDatabaseEnabled(true);
+        mWebViewHidden.getSettings().setDatabasePath(cachePath);
+        //开启 Application Caches 功能 
+        mWebViewHidden.getSettings().setAppCacheEnabled(true);
+
+        mWebViewHidden.getSettings().setAllowFileAccess(true);
+    }
+
+    private void initWebView2() {
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE); //设置 缓存模式 
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         //开启 DOM storage API 功能 
         mWebView.getSettings().setDomStorageEnabled(true);
 
+        String cachePath = getCacheDir().getAbsolutePath();
         //设置  Application Caches 缓存目录 
-        mWebView.getSettings().setAppCachePath(getExternalCacheDir().getAbsolutePath());
+        mWebView.getSettings().setAppCachePath(cachePath);
+        mWebView.getSettings().setDatabaseEnabled(true);
+        mWebView.getSettings().setDatabasePath(cachePath);
         //开启 Application Caches 功能 
         mWebView.getSettings().setAppCacheEnabled(true);
+
+        mWebView.getSettings().setAllowFileAccess(true);
     }
 
+    private void setClient() {
+        mWebViewHidden.setWebChromeClient(new WebChromeClient());
+        mWebViewHidden.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Logger.d("load start");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Logger.d("load finished");
+                //mWebView.loadUrl(url);
+            }
+        });
+    }
+
+    private void setClient2() {
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Logger.d("webview2 load start");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Logger.d("webview2 load finished");
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+       /* mContentLayout.removeView(mWebView);
+        mContentLayout.removeView(mWebViewHidden);
+        mWebView.destroy();
+        mWebViewHidden.destroy();*/
+    }
 }
--- app/src/main/java/com/okry/newstuff/util/Util.java
+++ app/src/main/java/com/okry/newstuff/util/Util.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
@@ -46,5 +48,42 @@ public class Util {
             drawable.draw(canvas);
             return bitmap;
         }
+    }
+
+    /**
+     * 是否有可用的3G、Wifi网络
+     *
+     * @return 是否有可用的3G、Wifi网络
+     */
+    public static boolean isAvailableNetWork(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        return isAvailableNetWork(cm);
+    }
+
+    /**
+     * 是否有可用的3G、Wifi网络
+     *
+     * @param cm ConnectivityManager
+     * @return 是否有可用的3G、Wifi网络
+     * @author liubo
+     */
+    public static boolean isAvailableNetWork(ConnectivityManager cm) {
+        //可用网络判断
+        NetworkInfo netWifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (netWifi != null) {
+            if (netWifi.isConnected()) {
+                return true;
+            }
+        }
+
+        NetworkInfo netMobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (netMobile != null) {
+            if (netMobile.isConnected()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }

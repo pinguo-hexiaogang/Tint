cc8be80533a5a085e0012b7b85097dfb6fc2466e
hexiaogang <hexiaogang@camera360.com>
2016-12-17 10:20:02 +0800

    [何小刚]kk

--- app/src/main/java/com/okry/newstuff/activity/OkHttpActivity.java
+++ app/src/main/java/com/okry/newstuff/activity/OkHttpActivity.java
@@ -1,5 +1,6 @@
 package com.okry.newstuff.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -20,6 +21,17 @@ public class OkHttpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ok_http);
         request();
+
+    }
+
+    public void uriTest() {
+        String testStr = "test://xiaogang@google.com:899/hexiaogang/kjkkk";
+        Uri uri = Uri.parse(testStr);
+        String schme = uri.getScheme();
+        String host = uri.getHost();
+        String path = uri.getPath();
+        String au = uri.getAuthority();
+        System.out.print("schkkkk");
     }
 
     private void request() {
--- app/src/main/java/com/okry/newstuff/activity/RippleActivity.java
+++ app/src/main/java/com/okry/newstuff/activity/RippleActivity.java
@@ -1,10 +1,14 @@
 package com.okry.newstuff.activity;
 
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 
 import com.okry.newstuff.R;
@@ -18,8 +22,11 @@ public class RippleActivity extends ActionBarActivity {
     ImageView mImageView;
     @InjectView(R.id.ripple_image_view)
     View mRippleView;
+    @InjectView(R.id.trans_layout)
+    View mTransLayout;
 
     private int a = 0xffffffff;
+
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,30 @@ public class RippleActivity extends ActionBarActivity {
         ButterKnife.inject(this);
         //RippleDrawable rippleDrawable = new RippleDrawable(ColorStateList.valueOf(getResources().getColor(R.color.light_common_high_light)),mImageView.getDrawable(),null);
         //mImageView.setImageDrawable(rippleDrawable);
+
+        //mTransLayout.setTranslationX(500);
         Logger.d("a:" + a);
+        int test = finallyTest();
+        System.out.println("test is:" + test);
+    }
+
+    private int finallyTest() {
+        int c = 1;
+        try {
+            c++;
+            System.out.println("try执行中...");
+            int a = 1/0;
+            //return c + 100; //--------1
+        } catch (Exception e) {
+            System.out.println("exception执行中...");
+            c = c + 200;
+            return c;   //--------4
+        } finally {
+            c = c + 1000;
+            System.out.println("finally执行中...");
+            //return c;  //--------2
+        }
+        c = c+800;
+        return c;   //---------3
     }
 }
